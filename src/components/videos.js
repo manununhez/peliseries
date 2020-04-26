329399db5eb42bce38bd3187a304029c9e953a67
Manuel Nunez <03.manu@gmail.com>
2020-04-26 15:35:36 +0200

    Improvements of the UI

--- src/components/videos.js
+++ src/components/videos.js
@@ -27,22 +27,29 @@ function createVideos(videos) {
   for (let i = index_init; i < videos.title.length; i++) {
     children.push(
 
-      <Col className="py-2 px-md-4">
+      <Col className="py-2 px-md-4" sm>
         <Card className="bg-secondary shadow border-0">
-          <CardBody >
-            <a href={videos.video_url[i]}><img className="img-fluid rounded shadow-lg" src={videos.thumbnail[i]} alt="" /></a>
 
+          <CardBody>
             <Container>
               <Row className="justify-content-center">
-              <h6 className="mb-0 al"><a href={videos.video_url[i]} className="title">{videos.title[i]}</a></h6>
+                <a href={videos.video_url[i]}><img className="img-fluid rounded shadow-lg" src={videos.thumbnail[i]} alt="" /></a>
               </Row>
-              <Row>
-                <Col >
-                <h6 className="small mb-0">Temporada {videos.season[i]}</h6>
+
+              <Row className="justify-content-center">
+                <h6 className="mb-0 al"><a href={videos.video_url[i]} className="title">{videos.title[i]}</a></h6>
+              </Row>
+              <Row className="justify-content-center">
+                <ul className="list-inline">
+                  <li className="list-inline-item">Temporada {videos.season[i]}</li>
+                  <li className="list-inline-item">Episodio {videos.episode[i]}</li>
+                </ul>
+                {/* <Col>
+                  <h6 className="small mb-0">Temporada {videos.season[i]}</h6>
                 </Col>
-                <Col >
-                <h6 className="small mb-0">Episodio {videos.episode[i]}</h6>
-                </Col>
+                <Col>
+                  <h6 className="small mb-0">Episodio {videos.episode[i]}</h6>
+                </Col> */}
               </Row>
             </Container>
           </CardBody>

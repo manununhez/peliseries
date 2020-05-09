4a7eca07c145a34eeb95956c8e006102c0d14dd9
Manuel Nunez <03.manu@gmail.com>
2020-05-09 15:03:37 +0200

    Added season selector

--- src/components/videos.js
+++ src/components/videos.js
@@ -23,26 +23,25 @@ const Videos = ({ videos }) => {
 
 function createVideos(videos) {
   let children = []
-  var index_init = 1;//from 1, to avoid the header name of the cell. e.g.,videos.id[0] == title
-  for (let i = index_init; i < videos.title.length; i++) {
+  for (let i = 0; i < videos.length; i++) {
     children.push(
 
-      <Col className="py-2 px-md-4" sm>
+      <Col className="py-4 px-md-4" sm>
         <Card className="bg-secondary shadow border-0">
 
           <CardBody>
             <Container>
               <Row className="justify-content-center">
-                <a href={videos.video_url[i]}><img className="img-fluid rounded shadow-lg" width="400" src={videos.thumbnail[i]} alt="" /></a>
+                <a href={videos[i].video_url}><img className="img-fluid rounded shadow-lg" width="400" src={videos[i].thumbnail} alt="" /></a>
               </Row>
 
               <Row className="justify-content-center">
-                <h5 className="mb-0 al"><a href={videos.video_url[i]} className="title">{videos.title[i]}</a></h5>
+                <h5 className="mb-0 al"><a href={videos[i].video_url} className="title">{videos[i].title}</a></h5>
               </Row>
               <Row className="justify-content-center">
                 <ul className="list-inline">
-                  <li className="list-inline-item">Temporada {videos.season[i]}</li>
-                  <li className="list-inline-item">Episodio {videos.episode[i]}</li>
+                  <li className="list-inline-item">Temporada {videos[i].season}</li>
+                  <li className="list-inline-item">Episodio {videos[i].episode}</li>
                 </ul>
                 {/* <Col>
                   <h6 className="small mb-0">Temporada {videos.season[i]}</h6>
--- src/views/movies.js
+++ src/views/movies.js
@@ -5,8 +5,148 @@ import {
 } from "reactstrap";
 import Videos from '../components/videos';
 
+import {
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+} from "reactstrap";
 
 class Movies extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: [{
+        season: "",
+        data: [{
+          id: "",
+          title: "",
+          season: "",
+          episode: "",
+          description: "",
+          video_url: "",
+          created: "",
+          thumbnail: "",
+          duration: ""
+        }]
+      }],
+      selectedSeason: {
+        season: "",
+        data: [{
+          id: "",
+          title: "",
+          season: "",
+          episode: "",
+          description: "",
+          video_url: "",
+          created: "",
+          thumbnail: "",
+          duration: ""
+        }]
+      }
+    };
+
+    this.handleDropdownClick = this.handleDropdownClick.bind(this)
+
+  }
+
+  componentDidMount() {
+
+    const url = this.props.location.state === undefined ? localStorage.getItem('url') : this.props.location.state.url;
+
+    localStorage.setItem('url', url);
+
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => {
+        var entry = data.feed.entry;
+        var id = []; // the leftmost column of the Google Sheets
+        var season = []; // second-left column
+        var episode = [];// third-left column
+        var title = [];// fourth-left column
+        var description = [];// fifth-left column
+        var video_url = [];// sixth-left column
+        var created = [];// eighth-left column
+        var thumbnail = [];// nineth-left column
+        var duration = [];// last column
+        for (var i = 0; i < entry.length; i += 9) {
+          // entry[i].content.$t retrieves the content of each cell
+          id.push(entry[i].content.$t);
+          season.push(entry[i + 1].content.$t);
+          episode.push(entry[i + 2].content.$t);
+          title.push(entry[i + 3].content.$t);
+          description.push(entry[i + 4].content.$t);
+          video_url.push(entry[i + 5].content.$t);
+          created.push(entry[i + 6].content.$t);
+          thumbnail.push(entry[i + 7].content.$t);
+          duration.push(entry[i + 8].content.$t);
+        }
+
+        if (season.length > 0) { //group movies by seasons
+          let firstSeason = season[1];
+          let movie = [{
+            season: firstSeason,
+            data: []
+          }]
+          let movieIndex = 0
+          for (var i = 1; i < season.length; i++) {
+
+            if (firstSeason === season[i]) {
+              movie[movieIndex].data.push({
+                id: id[i],
+                title: title[i],
+                season: season[i],
+                episode: episode[i],
+                description: description[i],
+                video_url: video_url[i],
+                created: created[i],
+                thumbnail: thumbnail[i],
+                duration: duration[i]
+              });
+            } else {
+              firstSeason = season[i];
+
+              movie.push({
+                season: firstSeason,
+                data: [{
+                  id: id[i],
+                  title: title[i],
+                  season: season[i],
+                  episode: episode[i],
+                  description: description[i],
+                  video_url: video_url[i],
+                  created: created[i],
+                  thumbnail: thumbnail[i],
+                  duration: duration[i]
+                }]
+              });
+
+              movieIndex++;
+
+            }
+          }
+
+
+          this.setState({ movie: movie, selectedSeason: movie[0] });
+        }
+      })
+      .catch(console.log)
+  }
+
+  handleDropdownClick(selectedSeason) {
+    console.log(selectedSeason)
+    //update screen with the selected season
+    for (let i = 0; i < this.state.movie.length; i++) {
+      if (selectedSeason === this.state.movie[i].season) {
+        this.setState({ selectedSeason: this.state.movie[i] });
+        break;
+      }
+    }
+    console.log(this.state)
+
+  }
+
   render() {
     return (
       <>
@@ -30,11 +170,23 @@ class Movies extends Component {
               </div>
 
               <div className="pt-4 text-center">
-                <h4 className="display-4 mb-0">{this.state.id[1]}</h4>
+                <h4 className="display-4 mb-0">{this.state.selectedSeason.data[0].id}</h4>
               </div>
-              <Container className="shape-container d-flex align-items-center py-lg">
+              
+              
+              <Container style={{overflow: 'visible'}}>
+              <Row className="align-items-center justify-content-center">
+                  <UncontrolledDropdown>
+                    <DropdownToggle caret color="secondary">
+                    Temporada {this.state.selectedSeason.season}
+                </DropdownToggle>
+                    <DropdownMenu style={{float:'inherit'}}>
+                      {loadSeasons(this.state.movie, this.handleDropdownClick, this.state.selectedSeason.season)}
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Row>
                 <Row className="align-items-center justify-content-center">
-                  <Videos videos={this.state} />
+                  <Videos videos={this.state.selectedSeason.data} />
                 </Row>
               </Container>
             </section>
@@ -44,72 +196,20 @@ class Movies extends Component {
     )
   }
 
+}
 
-  state = {
-    id: [],
-    title: [],
-    season: [],
-    episode: [],
-    description: [],
-    video_url: [],
-    created: [],
-    thumbnail: [],
-    duration: []
-  };
+function loadSeasons(movie, handleDropdownClick, selectedSeason) {
+  let children = []
 
-  componentDidMount() {
-
-    // console.log(this.props.location.state);
-
-    const url = this.props.location.state === undefined ? localStorage.getItem('url') : this.props.location.state.url;
-
-    // const url = name ? localStorage.getItem('url') : name;
-
-    localStorage.setItem('url', url);
-
-    fetch(url)
-      .then(res => res.json())
-      .then((data) => {
-        var entry = data.feed.entry;
-        var id = []; // the leftmost column of the Google Sheets
-        var season = []; // second-left column
-        var episode = [];// third-left column
-        var title = [];// fourth-left column
-        var description = [];// fifth-left column
-        var video_url = [];// sixth-left column
-        var html = [];// seventh-left column
-        var created = [];// eighth-left column
-        var thumbnail = [];// nineth-left column
-        var duration = [];// last column
-        for (var i = 0; i < entry.length; i += 9) {
-          // entry[i].content.$t retrieves the content of each cell
-          id.push(entry[i].content.$t);
-          season.push(entry[i + 1].content.$t);
-          episode.push(entry[i + 2].content.$t);
-          title.push(entry[i + 3].content.$t);
-          description.push(entry[i + 4].content.$t);
-          video_url.push(entry[i + 5].content.$t);
-          created.push(entry[i + 6].content.$t);
-          thumbnail.push(entry[i + 7].content.$t);
-          duration.push(entry[i + 8].content.$t);
-        }
-
-        // console.log(id);
-        // console.log(season);
-        // console.log(episode);
-        // console.log(title);
-        // console.log(reach);
-        // console.log(imp);
-        // console.log(avg);
-
-        this.setState({
-          id: id, title: title, season: season, episode: episode,
-          description: description, video_url: video_url,
-          html: html, created: created, thumbnail: thumbnail, duration: duration
-        })
-      })
-      .catch(console.log)
+  for (let i = 0; i < movie.length; i++) {
+    children.push(
+      <DropdownItem active={selectedSeason === movie[i].season} onClick={handleDropdownClick.bind(this, movie[i].season)}>
+        {movie[i].season}
+      </DropdownItem>
+    );
   }
+  return (children);
+
 }
 
 export default Movies;
